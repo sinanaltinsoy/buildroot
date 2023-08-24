95f3bf52e67250e85718511e5abb9c72536760f7
sinanaltinsoy <altinsoy.snn@gmail.com>
2023-08-24 16:13:17 +0200

    change makefile for package

--- external_packages/package/socket-package/socket-package.mk
+++ external_packages/package/socket-package/socket-package.mk
@@ -1,15 +1,15 @@
-SOCKET_PACKAGE_VERSION = 2bc727cef66af65a6816349a0d990f272c41b4ec
-SOCKET_PACKAGE_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-sinanaltinsoy.git
+SOCKET_PACKAGE_VERSION = 72a35bd77f1f96b3f1d4af6ee51b76273cb09494
+SOCKET_PACKAGE_SITE = git@github.com:sinanaltinsoy/buildroot.git
 SOCKET_PACKAGE_SITE_METHOD = git
 SOCKET_PACKAGE_GIT_SUBMODULES = YES
 
 define SOCKET_PACKAGE_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/app all
 endef
 
 define SOCKET_PACKAGE_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -m 0755 $(@D)/app/socket_package $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/app/socket_package.sh $(TARGET_DIR)/etc/init.d/S99socketpackage
 endef
 
 $(eval $(generic-package))
